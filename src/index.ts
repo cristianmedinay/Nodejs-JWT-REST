1ff2af34aa386895e0c9daa156e6784dcfe69342
cristian <cristianmedinay@gmail.com>
2023-09-17 15:39:44 +0200

    gitignore

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './app'
-import dbInit from './database'
-const port =  process.env.PORT || 3000
-dbInit()
-app.listen(port,()=>console.log(`Listo por el puerto ${port}`));
-
-/* app.listen(app.get('port'));console.log('server on port', app.get('port'))  */
